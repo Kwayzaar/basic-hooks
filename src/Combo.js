7d42155a7190e0ab3ca8073df1b3808243b868cd
andy-aguilar <aaguil3@gmail.com>
2021-04-12 16:39:50 -0400

    Add combo component

---
+++ src/Combo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const quoteURL = "https://api.taylor.rest/"
+const imgURL = "https://api.taylor.rest/image"
+
+export default function Combo() {
+
+
+    return (
+        <div>
+            <p>Count: </p>
+            <button onClick={() => console.log("!")}>Increase Count</button>
+            
+        </div>
+    )
+}
