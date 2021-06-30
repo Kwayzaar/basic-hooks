3e5925da13b13cda718bd8cfcebdce6779b68b89
Eric <lee.eric.03@gmail.com>
2021-06-30 16:32:22 -0600

    getting started

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function Counter() {
 
+    const [count, setCount] = useState(0)
+    
     const handleIncrease = () => {
-        console.log("increase my state")
+        setCount(count + 1)
     }
+
 
 
     return (
         <div>
-            <p>Count: </p>
+            <p>Count: {count} </p>
             <button onClick={handleIncrease}>Increase Count</button>
-            <button onClick={() => console.log("decrease my state")}>Decrease Count</button>
+            <button onClick={() => setCount(count - 1)}>Decrease Count</button>
         </div>
     )
 }
--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const quoteURL = "https://api.taylor.rest/"
 const imgURL = "https://api.taylor.rest/image"
 
 export default function Data() {
+    const [quote,setQuote] = useState("")
+    const [image, setImage] = useState("")
     
+    const fetchQuote = () => {
+        fetch(quoteURL)
+        .then(resp => resp.json())
+        .then(quote => setQuote(quote.quote))
+    }
+
+    const fetchImage = () => {
+        fetch(imgURL)
+        .then(resp => resp.json())
+        .then(quote => setImage(image.url))
+    }
 
     return (
         <div>
